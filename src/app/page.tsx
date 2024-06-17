82f7f475e44b8b77c08b843086588798c8e1ea50
Tony Lam <Tonylam750outlook.com>
2024-06-17 12:50:00 +0200

    måtte tydeligvis bytte til tailwind sin skrivemåte

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,22 @@ import React from "react";
 
 export default function Home() {
   return (
-   <body>
-    <div >
+    <>
+      <header >
+        <div className="border-b-4 border-lime-300 shadow-md shadow-lime-300 h-44 w-full relative"  >
+          <img src="https://i.pinimg.com/564x/5f/5f/76/5f5f76ff3724b5890afaafb978ae856d.jpg" className="w-full h-full object-cover absolute mix-blend-overlay" />
+       
+          <div className="p-16">
+            <h1 className=" text-white text-6xl font-bold">Goblin Hut</h1>
+          </div>
+        </div> 
+      </header> 
 
-      <h1 className="header">GoblinHut</h1>
-      <Link href="/hut" className="link">The Hut</Link>
-      
-      
-    </div>
-  </body>
+      <main className="bg-black h-screen">
+        <h1 className=" text-white font-serif">Hva i all verden</h1>
+        <p className=" text-white font-serif">må bytte bakgrunn</p>
+      </main>
+    </>  
   );
 }
- 
+                   

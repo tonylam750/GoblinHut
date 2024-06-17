a269755b1288a64215467a98e1b940c66709315e
Tony Lam <Tonylam750@outlook.com>
2024-06-17 23:41:08 +0200

    new bg image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
   return (
     <>
       <header>
-        <div className="border-b-4 border-lime-300 shadow-md shadow-lime-300 h-44 w-full relative"  >
-          <img src="https://i.imgur.com/nkW5pAB.jpeg" className="w-full h-full  object-cover absolute mix-blend-overlay resize-none" />
+        <div className="border-b-4 border-blue-300 shadow-md shadow-blue-300 h-96 w-full relative"  >
+          <img src="https://i.imgur.com/gXbyeq4.jpeg" className="w-full h-full  object-fit absolute mix-blend-overlay resize-none" />
        
           <div className="p-16">
-            <h1 className=" text-white text-6xl font-bold">Goblin Hut</h1>
+            <h1 className=" text-green-300 text-6xl font-bold italic ">Goblin Hut</h1>
           </div>
         </div> 
       </header> 
